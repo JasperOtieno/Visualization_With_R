b996ba18785cf23d059ae15a5921f27b555efec5
JasperOtieno <jaotie@gmail.com>
2025-02-02 18:27:46 +0300

    upadated

--- app.R
+++ app.R
@@ -4,6 +4,4 @@
 # Date: 10Dec2023
 ################################################################################
 
-#load packages----
-source("Scripts/install_packages.R")
 shinyApp(ui = ui, server = server)
